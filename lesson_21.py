57cd4ae5900fd8172e689735f1f261a402cc6bf4
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-08-02 15:51:32 +0300

    second commit

--- lesson_21.py
+++ lesson_21.py
@@ -1,26 +1,21 @@
-def TransformTransform(A, N):
+def Transform(A):
   b = []
-  B = []
-  S = 0
-  k = 0
   m = 0
-  summ = 0
-  res = False
-  if N == len(A):
-    for i in range(N):
-      for j in range(0, len(A) - i):
+  for i in range(len(A)):
+      for j in range(0, len(A) - i - 1):
         k = i + j
         for z in range(j,k):
           if A[z] > m:
             m = A[z]
         b.append(m)
-    for i in range(len(b)):
-      for j in range(0, len(b) - i):
-        k = i + j
-        for z in range(j,k):
-          if b[z] > m:
-            m = b[z]
-        B.append(m)
+  return b
+def TransformTransform(A, N):
+  B = []
+  summ = 0
+  res = False
+  if N == len(A):
+    B = Transform(A)
+    B = Transform(B)
     for i in range(len(B)):
       summ = summ + B[i]
     if summ%2 == 0:
