ff300c051ffd54c9420fc959929c085fe7c40fa6
AlfaBettaGamma <surmenko_konstantin@mail.ru>
2019-08-02 20:27:45 +0300

    update transformation

--- lesson_21.py
+++ lesson_21.py
@@ -1,25 +1,28 @@
 def Transform(A):
   b = []
-  m = 0
+  k = 0
   for i in range(len(A)):
-      for j in range(0, len(A) - i - 1):
-        k = i + j
-        for z in range(j,k):
-          if A[z] > m:
-            m = A[z]
-        b.append(m)
+    for j in range(0,len(A)-i):
+      k = i + j
+      m = 0
+      for z in range(j,k+1):
+        if m <= A[z]:
+          m = A[z]
+      b.append(m)
   return b
+
 def TransformTransform(A, N):
   B = []
   summ = 0
   res = False
-  if N == len(A):
-    B = Transform(A)
-    B = Transform(B)
-    for i in range(len(B)):
-      summ = summ + B[i]
-    if summ%2 == 0:
-      res = True
-      return res
-  else:
-    return 'Неверно введены данные!'
+  B = Transform(A)
+  print(len(B))
+  B = Transform(B)
+  print(len(B))
+  for i in range(len(B)):
+    summ = summ + B[i]
+  print(summ)
+  if summ%2 == 0 and summ > 0:
+    res = True
+  print(res)
+  return res
--- test.py
+++ test.py
@@ -1,30 +1,31 @@
 def Transform(A):
   b = []
-  m = 0
+  k = 0
   for i in range(len(A)):
-      for j in range(0, len(A) - i - 1):
-        k = i + j
-        for z in range(j,k):
-          if A[z] > m:
-            m = A[z]
-        b.append(m)
+    for j in range(0,len(A)-i):
+      k = i + j
+      m = 0
+      for z in range(j,k+1):
+        if m <= A[z]:
+          m = A[z]
+      b.append(m)
   return b
+
 def TransformTransform(A, N):
   B = []
   summ = 0
   res = False
-  if N == len(A):
-    B = Transform(A)
-    B = Transform(B)
-    for i in range(len(B)):
-      summ = summ + B[i]
-    if summ%2 == 0:
-      res = True
-      return res
-  else:
-    return 'Неверно введены данные!'
+  B = Transform(A)
+  print(len(B))
+  B = Transform(B)
+  print(len(B))
+  for i in range(len(B)):
+    summ = summ + B[i]
+  print(summ)
+  if summ%2 == 0 and summ > 0:
+    res = True
+  print(res)
+  return res
 
-
-
-print(TransformTransform([1,2,3,4,5,6,7],7))
-print(TransformTransform([1,2,3,4,5],5))
+#TransformTransform([1,2,3],3)
+TransformTransform([1,2,1,7,2,4,3,1,5,1,2,1,6,1,2],15)
